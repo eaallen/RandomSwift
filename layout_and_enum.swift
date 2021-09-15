f5b26e8914caf8053c56eba9e039a8ca5806ff55
eaallen <elijah.a.allen@gmail.com>
2021-09-14 19:38:41 -0600

    .

--- layout_and_enum.swift
+++ layout_and_enum.swift
@@ -1,3 +1,35 @@
 /*********************************************************************
 
+layout priority
+1. image
+2. text
+3. anything flexable
+
+Stack View elelms
+- Spacer
+- Divider
+
+layout priotoirty ==> gets the most space
+
+
+padding redues the amoutn of space to children 
+
+
+custom views want to fill and look good
+--- GeometryReader
+  - focus on size
+
+handle safe areas to get to the edge of the screen
+
+PinnedView 
+
+content
+
+
+
+
 *********************************************************************/
+
+
+// DEMO 
+
